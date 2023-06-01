ca0fbde7988bf8d0fc4e8c91f927500a25d3afce
Jzridar <jzridar@gmail.com>
2023-05-31 22:25:36 -0400

    Add link to products page in home page

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -4,14 +4,13 @@ import {
 } from "react-bootstrap";
 
 import { LinkContainer } from "react-router-bootstrap";
-// import { Link, useNavigate } from "react-router-dom";
 
 const HeaderComponent = () => {
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <LinkContainer to="/">
+        <LinkContainer to="/admin/products">
           <Navbar.Brand href="/">BEST ONLINE SHOP</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
